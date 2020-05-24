b6a65cf83cd61b89c7d0f6bb03e634a439258f10
Ayush Rastogi <ayush.rastogi@daalchini.co.in>
2020-05-24 16:08:02 +0530

    Added - custom Toast

--- Toast/app/src/main/java/com/geminisoftservices/toast/MainActivity.java
+++ Toast/app/src/main/java/com/geminisoftservices/toast/MainActivity.java
@@ -3,17 +3,41 @@ package com.geminisoftservices.toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCustomToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // default Toast
         Toast.makeText(getApplicationContext(),"Hello world!", Toast.LENGTH_SHORT).show();
+
+        // link your backend button object with your front-end button id
+        btnCustomToast = findViewById(R.id.btnCustomToast);
+
+        // click event of button
+
+        btnCustomToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = getLayoutInflater();
+                View toastLayout = inflater.inflate(R.layout.custom_toast, (ViewGroup) findViewById(R.id.llCustom));
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(toastLayout);
+                toast.show();
+            }
+        });
+
 
 
 
