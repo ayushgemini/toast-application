cb330c341e97c04eda1967b58e4f49afc48c198c
Ayush Rastogi <ayush.rastogi@daalchini.co.in>
2020-05-24 15:24:26 +0530

    Added - Default Toast Added

---
+++ Toast/app/src/main/java/com/geminisoftservices/toast/MainActivity.java
@@ -0,0 +1,21 @@
+package com.geminisoftservices.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(getApplicationContext(),"Hello world!", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+}
